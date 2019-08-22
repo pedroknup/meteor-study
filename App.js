abf48fcceb6b8e896cdebbc506fe6a291fb0bacb
Pedro Knup <pk@MacBook-Pro-van-Pedro.local>
2019-08-22 19:35:41 +0200

    fix: webview fixed

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `
+const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); true `
 
 const App = () => {
   return (
@@ -32,10 +32,7 @@ const App = () => {
       <View style={styles.container}>
         <WebView
           source={{uri: 'http://localhost:3000'}}
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          scalesPageToFit={isAndroid() ? false : true}
-          injectedJavaScript={INJECTEDJAVASCRIPT}
+          useWebKit={true}
           scrollEnabled
         />
       </View>
